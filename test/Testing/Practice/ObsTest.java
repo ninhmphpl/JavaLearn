39da2bec999090c354ef2adde6e986aab8ea4bed
NinhPc <ninhmphpl@gmail.com>
2022-10-17 15:39:20 +0700

    up58-394687918

--- test/Testing/Practice/ObsTest.java
+++ test/Testing/Practice/ObsTest.java
@@ -1,7 +1,6 @@
 package Testing.Practice;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
