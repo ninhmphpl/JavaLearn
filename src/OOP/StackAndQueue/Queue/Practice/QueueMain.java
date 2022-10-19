70753fbb3615301c0e393e6e5be4d9f65137e7d2
NinhPc <ninhmphpl@gmail.com>
2022-10-19 14:23:38 +0700

    g87sdgfweasd23

---
+++ src/OOP/StackAndQueue/Queue/Practice/QueueMain.java
@@ -0,0 +1,16 @@
+package OOP.StackAndQueue.Queue.Practice;
+
+public class QueueMain {
+    public static void main(String[] args) {
+        MyLinkedListQueue queue = new MyLinkedListQueue();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+        System.out.println("Dequeued item is " + queue.dequeue());
+    }
+
+}
